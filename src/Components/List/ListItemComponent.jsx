168156f4e47fe0ee92889f93277fb6dc63cc5ad1
JuanitoHe <jhernandezv1402@gmail.com>
2020-10-15 15:44:12 -0500

    Aniadiendo ultimo archivo

--- src/Components/List/ListItemComponent.jsx
+++ src/Components/List/ListItemComponent.jsx
@@ -3,9 +3,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
-console.log("public ur",  process.env.PUBLIC_URL)
+
 const descargarArchivo = (urlDelArchivo) => {
-  console.log("-------->", process.env.PUBLIC_URL+urlDelArchivo)
   window.open(process.env.PUBLIC_URL+urlDelArchivo, '_blank')
 }
 
