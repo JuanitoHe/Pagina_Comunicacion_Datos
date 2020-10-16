8d2c6fb07fb0a31d0369c1029bc75b2bf60f5d6e
JuanitoHe <jhernandezv1402@gmail.com>
2020-10-15 21:31:09 -0500

    Aniadi archivos

--- src/Utils/FileMap.js
+++ src/Utils/FileMap.js
@@ -1,8 +1,8 @@
 export default [
   {
-    nombreAlumno: 'Juan',
+    nombreAlumno: 'Hernández Villalobos Juan José',
     avatar: 'https://scontent.fmex5-1.fna.fbcdn.net/v/t1.0-9/79536493_2952530628143325_6477067332433739776_o.jpg?_nc_cat=105&_nc_sid=09cbfe&_nc_eui2=AeH6rnFjOxoswtYlEdSVt0nZPT3n2P3Ua8A9PefY_dRrwNjec7xLzsqFgT11z1BK5z4DFxead767-ZXjndyDuxUb&_nc_ohc=tcLIqherGCkAX_2oB-U&_nc_ht=scontent.fmex5-1.fna&oh=6734e103bb93c71c7cb2d8c6ffc669af&oe=5FAED889',
-    correo: 'juan@juan.com',
+    correo: 'jhernandezv1402@alumno.ipn.mx',
     tareas: [
       {
         nombre: 'T1_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
@@ -37,88 +37,173 @@ export default [
     ]
   },
   {
-    nombreAlumno: 'Jose',
-    avatar: 'jose.png',
-    correo: 'jose@jose.com',
+    nombreAlumno: 'Ariadna Rangel Mireles',
+    avatar: 'https://scontent.fmex16-1.fna.fbcdn.net/v/t1.0-9/81934199_157510065544018_8117656062531731456_n.jpg?_nc_cat=110&_nc_sid=174925&_nc_ohc=ytgTRBTn4isAX-7iE3c&_nc_ht=scontent.fmex16-1.fna&oh=c3df1dea27d4d8a4ada8726a63f60239&oe=5FACF393',
+    correo: 'ariadnamireles2511@gmail.com',
     tareas: [
       {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JOSEEEEEEEE',
+        nombre: 'tarea1_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
         fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JOSEEEEEEEE.pdf'
+        url: '/Files/tarea1_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
       },
       {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JOSEEEEEEEE',
+        nombre: 'tarea2_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
         fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JOSEEEEEEEE.pdf'
+        url: '/Files/tarea2_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
+      },
+      {
+        nombre: 'tarea3_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
+        fecha: '15/10/2020',
+        url: '/Files/tarea3_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
+      },
+      {
+        nombre: 'tarea4_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
+        fecha: '15/10/2020',
+        url: '/Files/tarea4_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
+      },
+      {
+        nombre: 'tarea5_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
+        fecha: '15/10/2020',
+        url: '/Files/tarea5_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
+      },
+      {
+        nombre: 'tarea6_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
+        fecha: '15/10/2020',
+        url: '/Files/tarea6_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
+      },
+      {
+        nombre: 'tarea7_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna',
+        fecha: '15/10/2020',
+        url: '/Files/tarea7_comunicaciodedatos_2CM42_RangelMirelesJoyceAriadna.pdf'
       },
     ]
   },
   {
-    nombreAlumno: 'Juan J',
-    avatar: 'juan.png',
-    correo: 'juan@juan.com',
+    nombreAlumno: 'Raúl Uriel García Santiago',
+    avatar: 'https://scontent.fmex33-1.fna.fbcdn.net/v/t1.0-9/121485418_3528830623850001_7314505424389920335_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_eui2=AeGs9w6UVCwCOuheWbiMDlxTsQIJQgWzo6exAglCBbOjp5Mr1eRV4Gkvy7TrbHEOr7GuLu-s8TMaZ0w4dUdHf2wv&_nc_ohc=XvxSAY7HERoAX_tlBRi&_nc_ht=scontent.fmex33-1.fna&oh=255461c4dd142b86f1f7a544492a28e2&oe=5FAE9C0E',
+    correo: 'rugs_99@outlook.com',
     tareas: [
       {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        nombre: 'T01_Comunicacion2CM42_Garcia_Raul_Concepto_ComunicaciondeDatos',
         fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+        url: '/Files/T01_Comunicacion2CM42_Garcia_Raul_Concepto_ComunicaciondeDatos.pdf'
       },
       {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        nombre: 'T02_Comunicacion2CM42_Garcia_Raul_Concepto_Telecomueinformatica',
         fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+        url: '/Files/T02_Comunicacion2CM42_Garcia_Raul_Concepto_Telecomueinformatica.pdf'
+      },
+      {
+        nombre: 'T03_Comunicacion2CM42_Garcia_Raul_Mensajes_Senales',
+        fecha: '15/10/2020',
+        url: '/Files/T03_Comunicacion2CM42_Garcia_Raul_Mensajes_Senales.pdf'
+      },
+      {
+        nombre: 'T04_Comunicacion2CM42_Garcia_Raul_GenerarLista_Asistencia',
+        fecha: '15/10/2020',
+        url: '/Files/T04_Comunicacion2CM42_Garcia_Raul_GenerarLista_Asistencia.pdf'
+      },
+      {
+        nombre: 'T05_Comunicacion2CM42_Garcia_Raul_Transmision',
+        fecha: '15/10/2020',
+        url: '/Files/T05_Comunicacion2CM42_Garcia_Raul_Transmision.pdf'
+      },
+      {
+        nombre: 'T07_Comunicacion2CM42_Garcia_Raul_SIMPLEXDUPLEX',
+        fecha: '15/10/2020',
+        url: '/Files/T07_Comunicacion2CM42_Garcia_Raul_SIMPLEXDUPLEX.pdf'
+      },
+    ]
+  },  {
+    nombreAlumno: 'TAREAS EN EQUIPO',
+    avatar: '',
+    correo: '',
+    tareas: [
+      {
+        nombre: 'T08_Comunicacion2CM42_EQUIPOAZUL_ASINCRONA_SINCRONA',
+        fecha: '15/10/2020',
+        url: '/Files/T08_Comunicacion2CM42_EQUIPOAZUL_ASINCRONA_SINCRONA.pdf'
+      },
+      {
+        nombre: 'T09_Comunicacion2CM42_EQUIPOAZUL_Transmision',
+        fecha: '15/10/2020',
+        url: '/Files/T09_Comunicacion2CM42_EQUIPOAZUL_Transmision.pdf'
       },
     ]
   },
-  {
-    nombreAlumno: 'Juan H',
-    avatar: 'juan.png',
-    correo: 'juan@juan.com',
-    tareas: [
-      {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
-        fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
-      },
-      {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
-        fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
-      },
-    ]
-  },
-  {
-    nombreAlumno: 'Juan U',
-    avatar: 'juan.png',
-    correo: 'juan@juan.com',
-    tareas: [
-      {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
-        fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
-      },
-      {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
-        fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
-      },
-    ]
-  },
-  {
-    nombreAlumno: 'Jua n',
-    avatar: 'juan.png',
-    correo: 'juan@juan.com',
-    tareas: [
-      {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
-        fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
-      },
-      {
-        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
-        fecha: '15/10/2020',
-        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
-      },
-    ]
-  }
+  // {
+  //   nombreAlumno: 'Hernández Villalobos Juan José',
+  //   avatar: '',
+  //   correo: 'jhernandezv1402@alumno.ipn.mx',
+  //   tareas: [
+  //     {
+  //       nombre: 'T1_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T1_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T2_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T2_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T3_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T3_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T6_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T6_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T7_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T7_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //   ]
+  // },
+  // {
+  //   nombreAlumno: 'Hernández Villalobos Juan José',
+  //   avatar: '',
+  //   correo: 'jhernandezv1402@alumno.ipn.mx',
+  //   tareas: [
+  //     {
+  //       nombre: 'T1_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T1_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T2_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T2_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T3_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T3_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T6_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T6_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //     {
+  //       nombre: 'T7_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+  //       fecha: '15/10/2020',
+  //       url: '/Files/T7_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+  //     },
+  //   ]
+  // },
+  
 ]
