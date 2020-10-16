42be1357916c6cff24b18fc7eac8c11e06928b65
JuanitoHe <jhernandezv1402@gmail.com>
2020-10-15 19:57:12 -0500

    Aniadi usuarios

--- src/Utils/FileMap.js
+++ src/Utils/FileMap.js
@@ -33,4 +33,72 @@ export default [
       },
     ]
   },
+  {
+    nombreAlumno: 'Juan',
+    avatar: 'juan.png',
+    correo: 'juan@juan.com',
+    tareas: [
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+    ]
+  },
+  {
+    nombreAlumno: 'Juan',
+    avatar: 'juan.png',
+    correo: 'juan@juan.com',
+    tareas: [
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+    ]
+  },
+  {
+    nombreAlumno: 'Juan',
+    avatar: 'juan.png',
+    correo: 'juan@juan.com',
+    tareas: [
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+    ]
+  },
+  {
+    nombreAlumno: 'Juan',
+    avatar: 'juan.png',
+    correo: 'juan@juan.com',
+    tareas: [
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+      {
+        nombre: 'T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN',
+        fecha: '15/10/2020',
+        url: '/Files/T5_COMUNICACION_DE_DATOS_2CM42_HERNANDEZ_VILLALOBOS_JUAN.pdf'
+      },
+    ]
+  }
 ]
